5f68519c0efc803811e6bca944047095d4daad3a
jstran9 <wptran58@gmail.com>
2019-10-04 17:46:30 -0700

    cors configuration modification.

--- src/main/java/tran/compbuildbackend/config/WebConfig.java
+++ src/main/java/tran/compbuildbackend/config/WebConfig.java
@@ -2,11 +2,13 @@ package tran.compbuildbackend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static tran.compbuildbackend.constants.mapping.MappingConstants.*;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
