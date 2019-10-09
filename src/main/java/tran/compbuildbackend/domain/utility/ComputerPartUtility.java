3ec6391009bfc2f1aa1967c70fa8068842d33d66
jstran9 <wptran58@gmail.com>
2019-10-08 18:11:53 -0700

    Creating methods to subtract and add to a computer build's total price.

--- src/main/java/tran/compbuildbackend/domain/utility/ComputerPartUtility.java
+++ src/main/java/tran/compbuildbackend/domain/utility/ComputerPartUtility.java
@@ -4,9 +4,15 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import tran.compbuildbackend.domain.computerbuild.ComputerPart;
+import tran.compbuildbackend.exceptions.request.GenericRequestException;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
 
+import static tran.compbuildbackend.constants.exception.ExceptionConstants.CANNOT_PARSE_PRICE;
 import static tran.compbuildbackend.constants.tests.TestUtility.COMPUTER_BUILD_ID_KEY;
 import static tran.compbuildbackend.constants.tests.TestUtility.PURCHASE_DATE_KEY;
 
@@ -55,5 +61,35 @@ public class ComputerPartUtility {
         computerPart.setPrice(price);
         computerPart.setOtherNote(otherNotes);
         return computerPart;
+    }
+
+    public static double addValues(double totalPrice, double addingPrice)  {
+        return performAdditionOrSubtraction(totalPrice, addingPrice, 1);
+    }
+
+    public static double subtractValues(double totalPrice, double subtractingPrice) {
+        return performAdditionOrSubtraction(totalPrice, subtractingPrice, 2);
+    }
+
+    private static double performAdditionOrSubtraction(double totalPrice, double otherPrice, int operationType) {
+        /*
+         * for now it is expected computer builds won't go past 6 digits because a build is assumed to be some thousands
+         * of dollars such as $3000.50 or so.
+         */
+        MathContext mathContext = new MathContext(10);
+        BigDecimal bdTotalPrice = new BigDecimal(totalPrice, mathContext);
+        BigDecimal bdOtherPrice = new BigDecimal(otherPrice, mathContext);
+        BigDecimal result = null;
+        if(operationType == 1) {
+            result = bdTotalPrice.add(bdOtherPrice);
+        } else if(operationType == 2) {
+            result = bdTotalPrice.subtract(bdOtherPrice);
+        }
+        String resultAsStr = NumberFormat.getCurrencyInstance().format(result);
+        try {
+            return NumberFormat.getCurrencyInstance().parse(resultAsStr).doubleValue();
+        } catch(ParseException ex) {
+            throw new GenericRequestException(CANNOT_PARSE_PRICE);
+        }
     }
 }
