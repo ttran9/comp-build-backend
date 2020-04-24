257de384d140358a03a4785ebd6fae235a79cdab
jstran9 <wptran58@gmail.com>
2020-04-24 00:20:15 -0700

    Modification to deploy to standalone tomcat server (instead of embedded tomcat server).

--- src/main/java/tran/compbuildbackend/CompBuildBackendApplication.java
+++ src/main/java/tran/compbuildbackend/CompBuildBackendApplication.java
@@ -2,12 +2,18 @@ package tran.compbuildbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CompBuildBackendApplication {
+public class CompBuildBackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CompBuildBackendApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CompBuildBackendApplication.class);
+    }
 }
