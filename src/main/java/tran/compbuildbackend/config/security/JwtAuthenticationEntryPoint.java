633b91f2ea3829da0923e1fefba22d7175802d90
ttran9 <toddtran9@gmail.com>
2021-07-21 00:23:45 -0700

    removing exception that is not thrown.

--- src/main/java/tran/compbuildbackend/config/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/tran/compbuildbackend/config/security/JwtAuthenticationEntryPoint.java
@@ -5,8 +5,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import tran.compbuildbackend.exceptions.security.InvalidLoginExceptionResponse;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,7 +16,7 @@ import java.io.IOException;
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
-                         AuthenticationException e) throws IOException, ServletException {
+                         AuthenticationException e) throws IOException {
         InvalidLoginExceptionResponse invalidLoginExceptionResponse = new InvalidLoginExceptionResponse();
         String jsonLoginResponse = new Gson().toJson(invalidLoginExceptionResponse);
 
